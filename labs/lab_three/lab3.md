5079452591fc24f429c74604c3209d9627e70413
Aaron Shapiro <aaron@shappiro.com>
2019-01-29 23:15:00 -0500

    lab 3

---
+++ labs/lab_three/lab3.md
@@ -0,0 +1,24 @@
+### Part 0 - Documentation
+1. [Interest Wiki](https://github.com/ashapir0/csci-open-source-software/wiki/Interests)
+
+2. ![Latex 1](./latex1.png)
+3. ![Latex 2](./latex2.png)
+
+### Part 1 - Community
+#### 1. Project selection
+1. [AutoGrading](https://rcos.io/projects/huimingcheng/autograding)
+2. There has been 12 contributors.
+3. 
+    - 761093 Lines of code have been contributed based on wc.
+    - [First Commit](https://github.com/HuimingCheng/AutoGrading/commit/b1b4078cba4e1b682c7e636a207768d1bf628d10)
+    - [Last Commit](https://github.com/HuimingCheng/AutoGrading/commit/762d6e06aac1050c9b6f3a47fddeb6e3ed828ee4)
+    - One branch (master).
+
+#### 2. Gitstats
+Compared to the other projects in our this project is on the older and larger side of things. With a high number of authors the most active of which are Hubert51 and Ruijie Geng.  It seems these authors are longtime maintainers of the project.
+
+[AutoGrading Visualized](https://youtu.be/0wFrWGZTeTU)
+### Part 2
+
+[Test](./test_markdown_unittest.py)
+[MD](./markdown.py)
---
+++ labs/lab_three/markdown.py
@@ -0,0 +1,48 @@
+"""
+ Markdown.py
+ 0. just print whatever is passed in to stdin
+ 0. if filename passed in as a command line parameter, 
+    then print file instead of stdin
+ 1. wrap input in paragraph tags
+ 2. convert single asterisk or underscore pairs to em tags
+ 3. convert double asterisk or underscore pairs to strong tags
+
+"""
+
+import fileinput
+import re
+
+def convertStrong(line):
+  line = re.sub(r'\*\*(.*)\*\*', r'<strong>\1</strong>', line)
+  line = re.sub(r'__(.*)__', r'<strong>\1</strong>', line)
+  return line
+
+def convertEm(line):
+  line = re.sub(r'\*(.*)\*', r'<em>\1</em>', line)
+  line = re.sub(r'_(.*)_', r'<em>\1</em>', line)
+  return line
+
+def convertH1(line):
+  line = re.sub(r'<h1>(.*)</h1>', r'#\1', line)
+  return line  
+  
+def convertH2(line):
+  line = re.sub(r'<h2>(.*)</h2>', r'##\1', line)
+  return line  
+
+def convertH3(line):
+  line = re.sub(r'<h3>(.*)</h3>', r'###\1', line)
+  return line  
+
+
+def convertH3(line):
+  line = re.sub(r'<h3>(.*)</h3>', r'###\1', line)
+  return line  
+  
+for line in fileinput.input():
+  line = line.rstrip() 
+  line = convertStrong(line)
+  line = convertEm(line)
+  line = convertH1(line)
+  print '<p>' + line + '</p>',
+
